ab34a0a2608b863a397612f5fff94fe526eacd69
yashkumar <goudayash875@gmail.com>
2023-10-15 08:50:52 +0530

    HI

---
+++ src/git1/DIvide.java
@@ -0,0 +1,43 @@
+package git1;
+
+public class DIvide {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              int a=15,b=5, c;
+              int result=0;   
+              c=a+b;
+              while(c>b)
+              {
+            	  c=c-b;
+            	  result++;
+              }
+              System.out.println(result);
+              int reminder=(result*b)-a;
+              System.out.println(reminder);
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+	}
+
+}
